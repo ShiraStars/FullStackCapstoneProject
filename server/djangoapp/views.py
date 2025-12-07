d79c9db87f10e0bd245932747b893cb5f16a3f39
ShiraStars <skrikvirnicks@gmail.com>
2025-12-07 14:47:52 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -120,14 +120,14 @@ def get_dealerships(request, state="All"):
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
-    
+
     if dealer_id:
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
 
         for review in reviews:
             response = analyze_review_sentiments(review["review"])
-            print (response)
+            print(response)
             response["sentiment"] = review["sentiment"]
         return JsonResponse({"status": 200, "dealer": reviews})
     else:
@@ -136,24 +136,25 @@ def get_dealer_reviews(request, dealer_id):
 
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
-    
+
     if dealer_id:
         endpoint = "/fetchDealers"+str(dealer_id)
         dealerships = get_request(endpoint)
         return JsonResponse({"status": 200, "dealer": dealerships})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
-    
+
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    
-    if(request.user.is_anonymous == False):
+
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
             response = post_review(data)
-            return JsonResponse({"status": 200})
-        except:
-            return JsonResponse({"status": 401,"message": "Error in posting review"})
+            return JsonResponse({"status": 200, "review": response})
+        except Exception as e:
+            return JsonResponse({"status": 401,
+                                 "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
