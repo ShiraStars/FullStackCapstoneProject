d539ab0dde106a0fdc1206d45ea12c78143064c2
ShiraStars <skrikvirnicks@gmail.com>
2025-12-07 16:50:54 +0200

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 # pylint: disable=line-too-long
 backend_url = os.getenv(
-    'backend_url', default="https://skrikvirnick-3030.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai")
+    'backend_url', default="https://skrikvirnick-3030.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai") # noqa: E501
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
