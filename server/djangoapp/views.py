fceb67496cc57dd5fb98cd745aff4a56ef70af09
ShiraStars <skrikvirnicks@gmail.com>
2025-12-07 14:33:57 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -58,9 +58,9 @@ def login_user(request):
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
-
-    logout(request) # Terminate user session
-    data = {"userName": ""} # Return empty username
+    
+    logout(request)  # Terminate user session
+    data = {"userName": ""}  # Return empty username
     return JsonResponse(data)
 
 
@@ -102,25 +102,14 @@ def registration(request):
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
-# def get_cars(request):
-#     count = CarMake.objects.filter().count()
-#     print(count)
-#     if(count == 0):
-#         initiate()
-#     car_models = CarModel.objects.select_related('car_make')
-#     cars = []
-#     for car_model in car_models:
-#         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
-#     return JsonResponse({"CarModels":cars})
-
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
 # def get_dealerships(request):
-# Update the `get_dealerships` render list of dealerships all by default, 
+# Update the `get_dealerships` render list of dealerships all by default,
 # particular state if state is passed
 
 def get_dealerships(request, state="All"):
-    if(state == "All"):
+    if (state == "All"):
         endpoint = "/fetchDealers"
     else:
         endpoint = "/fetchDealers/"+state
@@ -128,7 +117,7 @@ def get_dealerships(request, state="All"):
     return JsonResponse({"status": 200, "dealers": dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-def get_dealer_reviews(request,dealer_id):
+def get_dealer_reviews(request, dealer_id):
     if dealer_id:
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
